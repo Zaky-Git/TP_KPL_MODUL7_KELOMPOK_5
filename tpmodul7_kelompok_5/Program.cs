cf1b69eb152ac2bea3c2f1bf8e38159fa7c655ec
MUHAMMAD ZAKY FATHURAHIM <46282283+Zaky-Git@users.noreply.github.com>
2023-04-02 21:30:36 +0700

    Update Program.cs

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -4,13 +4,9 @@ KuliahMahasiswa1302213067 zaky1 = new KuliahMahasiswa1302213067();
 
 zaky1.ReadJson();
 
-using tpmodul7_kelompok_5;
-
 DataMahasiswa1302213067 zaky = new DataMahasiswa1302213067();
 
 zaky.ReadJSON();
-
-using tpmodul7_kelompok_5;
 
 KuliahMahasiswa1302213016 MhsReza2 = new KuliahMahasiswa1302213016();
 
